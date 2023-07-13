0b7c39b349d2fe82b59e03eac42da62df86c62e4
BerkovskiiAlex <berkovskii845@gmail.com>
2023-07-13 14:32:44 +0300

    добавлен плавный скролл

--- src/js/index.js
+++ src/js/index.js
@@ -35,8 +35,14 @@ const onLoadMoreBtnElClick = async event => {
           timeout: 5000,
         }
       );
-      return;
     }
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight * 3,
+      behavior: 'smooth',
+    });
   } catch (err) {
     console.log(err);
   }
